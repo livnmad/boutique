74f1b52d510e27b471587b1a8b724398c4357881
Derek <91619609+livnmad@users.noreply.github.com>
2025-11-22 15:41:43 -0500

    updates to configuration for elastic

--- client/src/components/Logo.tsx
+++ client/src/components/Logo.tsx
@@ -2,6 +2,6 @@ import React from 'react';
 
 export default function Logo() {
   return (
-      <img src="/images/olivias_bead_boutique_1.png" alt="Olivia's Bead Boutique" style={{maxWidth: '700px', maxHeight: '100px', display: 'block'}} />
+      <img src="/images/olivias_bead_boutique.png" alt="Olivia's Bead Boutique" style={{maxWidth: '700px', maxHeight: '100px', display: 'block'}} />
   );
 }
